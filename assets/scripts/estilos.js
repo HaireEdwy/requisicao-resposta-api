c622ddc5189fa1c41bd4b4b14255a42cbce57512
haireedwy <haire.edwy@hotmail.com>
2023-07-07 09:28:25 -0300

    styles, estilos e GET

---
+++ assets/scripts/estilos.js
@@ -0,0 +1,10 @@
+const styles =document.childElement('link');
+
+styles.Attribute('rel','stylesheet');
+styles.Attribute('href','./assets/styles/style.css');
+
+/* <link rel="stylesheet" href="./assets/styles/style.css"> */
+
+document.head.appendChild(styles)
+
+
